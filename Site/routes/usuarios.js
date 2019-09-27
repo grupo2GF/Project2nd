882aceb999d527f373c5177ab13bb70b6c342717
eduhkbr <edu.abreu09@hotmail.com>
2019-09-27 20:49:21 -0300

    configurações de banco em nodejs

--- Site/routes/usuarios.js
+++ Site/routes/usuarios.js
@@ -13,7 +13,7 @@ router.post('/entrar', function (req, res, next) {
     if (login == undefined || senha == undefined) {
       throw new Error(`Dados de login não chegaram completos: ${login} / ${senha}`);
     }
-    return banco.sql.query(`select * from usuario where login='${login}' and senha='${senha}'`);
+    return banco.sql.query(`select * from usuario where loginUsuario='${login}' and senhaUsuario='${senha}'`);
   }).then(consulta => {
 
     console.log(`Usuários encontrados: ${JSON.stringify(consulta.recordset)}`);
@@ -46,14 +46,14 @@ router.post('/cadastrar', function (req, res, next) {
 
   banco.conectar().then(() => {
     console.log(`Chegou p/ cadastro: ${JSON.stringify(req.body)}`);
-	nome = req.body.nome; // depois de .body, use o nome (name) do campo em seu formulário de login
     login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
     senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login
-    if (login == undefined || senha == undefined || nome == undefined) {
+    codFuncionario = req.body.codFuncionario;
+    if (login == undefined || senha == undefined || codFuncionario == undefined) {
 	  // coloque a frase de erro que quiser aqui. Ela vai aparecer no formulário de cadastro
-      throw new Error(`Dados de cadastro não chegaram completos: ${login} / ${senha} / ${nome}`);
+      throw new Error(`Dados de cadastro não chegaram completos: ${login} / ${senha} / ${codFuncionario}`);
     }
-    return banco.sql.query(`select count(*) as contagem from usuario where login = '${login}'`);
+    return banco.sql.query(`select count(*) as contagem from tbUsuario where loginUsuario = '${login}'`);
   }).then(consulta => {
 
 	if (consulta.recordset[0].contagem >= 1) {
@@ -73,7 +73,7 @@ router.post('/cadastrar', function (req, res, next) {
   }).finally(() => {
 	  if (cadastro_valido) {		  
 			  
-		banco.sql.query(`insert into usuario (nome, login, senha) values ('${nome}','${login}','${senha}')`).then(function() {
+		banco.sql.query(`insert into tbUsuario (loginUsuario, senhaUsuario, codFuncionario) values (${login}','${senha}','${codFuncionario}')`).then(function() {
 			console.log(`Cadastro criado com sucesso!`);
 			res.sendStatus(201); 
 			// status 201 significa que algo foi criado no back-end, 
