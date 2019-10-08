2b9f3f6d94623d81d544c1434a3ff45f75abcdbf
eduhkbr <edu.abreu09@hotmail.com>
2019-10-08 00:12:17 -0300

    vigesima quinta tentativa

---
+++ Site/routes/leituras.js
@@ -0,0 +1,103 @@
+// não mexa nestas 3 linhas!
+var express = require('express');
+var router = express.Router();
+var banco = require('../app-banco');
+// não mexa nessas 3 linhas!
+
+router.get('/ultimas', function (req, res, next) {
+  console.log(banco.conexao);
+  banco.conectar().then(() => {
+    var limite_linhas = 8;
+    return banco.sql.query(`select top ${limite_linhas} 
+                            temperatura, 
+                            umidade, 
+                            FORMAT(momento,'HH:mm:ss') as momento 
+                            from leitura order by id desc`);
+  }).then(consulta => {
+
+    console.log(`Resultado da consulta: ${JSON.stringify(consulta.recordset)}`);
+    res.send(consulta.recordset);
+
+  }).catch(err => {
+
+    var erro = `Erro na leitura dos últimos registros: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+router.get('/estatisticas', function (req, res, next) {
+  console.log(banco.conexao);
+
+  var estatisticas = {
+    temp_maxima: 0,
+    temp_minima: 0,
+    temp_media: 0
+  };
+
+  banco.conectar().then(() => {
+    return banco.sql.query(`
+        select 
+          max(temperatura) as temp_maxima, 
+          min(temperatura) as temp_minima, 
+          avg(temperatura) as temp_media 
+        from leitura
+        `);
+  }).then(consulta => {
+    estatisticas.temp_maxima = consulta.recordset[0].temp_maxima;
+    estatisticas.temp_minima = consulta.recordset[0].temp_minima;
+    estatisticas.temp_media = consulta.recordset[0].temp_media;
+    console.log(`Estatísticas: ${JSON.stringify(estatisticas)}`);
+    res.send(estatisticas);
+  }).catch(err => {
+
+    var erro = `Erro na leitura dos últimos registros: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+router.get('/tempo-real', function (req, res, next) {
+  console.log(banco.conexao);
+
+  var estatisticas = {
+    temperatura: 0,
+    umidade: 0
+  };
+
+  banco.conectar().then(() => {
+    return banco.sql.query(`
+        select top 1 temperatura, umidade from leitura order by id desc
+        `);
+  }).then(consulta => {
+
+    estatisticas.temperatura = consulta.recordset[0].temperatura;
+    estatisticas.umidade = consulta.recordset[0].umidade;
+    console.log(`Tempo real: ${JSON.stringify(estatisticas)}`);
+
+    res.send(estatisticas);
+
+  }).catch(err => {
+
+    var erro = `Erro na leitura dos registros de tempo real: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+});
+
+
+// não mexa nesta linha!
+module.exports = router;
